6104c606c75d3d151d0ffc1b0da6c15dc987e64a
Brian Wright <brwr@mit.edu>
2015-11-17 14:23:57 -0500

    Comm pseudocode

--- rock_comm.c
+++ rock_comm.c
@@ -2,4 +2,25 @@
 #include "rock_headers.h"
 #endif
 
+/*
+//PSEUDOCODE:
+{
+// TEAM_COMM_RATE? Once per second?
+  sendRF("X")
+  sendRF(X)
+  sendRF("Y")
+  sendRF(Y)
+  sendRF("H")
+  sendRF(Heading)
+  sendRF("x")
+  sendRF(PuckX)
+  sendRF("y")
+  sendRF(PuckY)
+}
 
+ISR(m2)
+{
+  // Get packets from team
+  // Get packets from JF
+}
+*/
