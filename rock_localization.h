c5143e60d1e1255791d5f429b9afbea3e18b31b4
Brian Wright <brwr@mit.edu>
2015-11-17 14:18:57 -0500

    Untested. Initial puckDistance. Code very unstable.

--- rock_localization.h
+++ rock_localization.h
@@ -2,5 +2,28 @@
 #define _rock_localization_h
 
 #endif
+//TODO Clean rock_localization.h
 
 int findPuckDistance(void);
+
+//TODO Make poll(x)IR extern and put in vars.h
+extern int pollLeftIR;
+extern int pollCenterIR;
+extern int pollRightIR;
+
+//TODO Lookup best max() and max3() functions!
+#define max3(a, b, c) max(a, max(b, c))
+#define max(a, b) (a>b)?a:b
+
+int highestIRPoll; //TODO extern?
+
+char puckIsLeft;
+char puckIsRight;
+#define IR_SCHMITT_LEVEL 0
+
+char turnServo;
+#define TURN_LEFT        -1
+#define TURN_RIGHT        1
+#define TURN_NOT_NEEDED   0
+
+#define PUCK_DISTANCE_GAIN  10
