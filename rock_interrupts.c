2eb6cd1b12cdc502a717f59e09a1a7eb799d3d53
Brian Wright <brwr@mit.edu>
2015-11-23 01:42:53 -0500

    ISR for mRF updates state variable

--- rock_interrupts.c
+++ rock_interrupts.c
@@ -11,5 +11,38 @@ ISR(ADC_vect) /*ADC conversion complete!*/
 //TODO Check if INT2 will interfere with other functionality!
 ISR(INT2_vect)
 {
-//TODO  m_rf_read(buffer, packet_length);
+  m_rf_read(bufferRF, RF_PACKET_LENGTH); 
+  switch((unsigned char)bufferRF[0]) /*rock_comm.h*/ {
+    case 0xA0: /*COMM TEST*/
+      state = GAMEPLAY_COMM_TEST;
+      break;
+    case 0xA1: /*PLAY*/
+      state = GAMEPLAY_PLAY_COMMAND;
+      break;
+    case 0xA2: /*GOAL A*/
+      //TODO Ensure team selection is correct
+      state = (TRUE) ? GAMEPLAY_TIMEOUT : GAMEPLAY_SCORED_GOAL;
+      break;
+    case 0xA3: /*GOAL B*/
+      //TODO
+      state = (TRUE) ? GAMEPLAY_TIMEOUT : GAMEPLAY_SCORED_GOAL;
+      break;
+    case 0xA4: /*PAUSE*/
+      state = GAMEPLAY_TIMEOUT;
+      break;
+    case 0xA5: /*HALFTIME*/
+      state = GAMEPLAY_HALFTIME;
+      break;
+    case 0xA6: /*GAME OVER*/
+      state = GAMEPLAY_NOT_PLAYING;
+      break;
+    case 0xA7: /*ENEMY POSITIONS*/
+      //TODO Viable to use enemy position?      
+//      enemyA[i] = ... 
+//      enemyX[i] = ...
+//      enemyY[i] = ...
+      break;
+    default:
+      break;
+  }
 }
