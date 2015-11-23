0b4e6d9fcc2ceb4b38b1131adee4bea8cbdeb637
Brian Wright <brwr@mit.edu>
2015-11-23 01:09:34 -0500

    mRF interrupt routine added (unused)

--- rock_interrupts.c
+++ rock_interrupts.c
@@ -7,3 +7,9 @@ ISR(ADC_vect) /*ADC conversion complete!*/
   clear(  SMCR, SE ); //Disable sleep mode
   // Have other routines look up the ADC register
 }
+
+//TODO Check if INT2 will interfere with other functionality!
+ISR(INT2_vect)
+{
+//TODO  m_rf_read(buffer, packet_length);
+}
