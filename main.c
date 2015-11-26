254ad7fe8866dbd4e2cc978b786be67103a54c1b
Brian Wright <brwr@mit.edu>
2015-11-26 16:31:10 -0500

    Link differential wheel motor PWM to angle error

--- main.c
+++ main.c
@@ -13,12 +13,29 @@
 #include "rock_headers.h"
 #endif
 
-void qualify(void); //TODO Delete after qualification!
+void qualify(void); //TODO Move function after qualification!
+void calculateAngleToGoal(void); //TODO Move function after qual
+
+//TODO
+extern unsigned char currentTeam;
 
 int main()
 {
   init(); //rock_init_routine.c
   
+  //TODO Which side of the rink does the robot start?
+  m_wait(500);
+  locationWhereAmI();
+#ifndef RED
+#define RED  1
+#define BLUE 2
+#endif
+  if (robotX < 511)
+  {
+    currentTeam = RED;
+  } else {
+    currentTeam = BLUE;
+  }
   while(1)
   {
 //    stateMachine(); //rock_state_machine.c
@@ -30,12 +47,14 @@ int main()
   }
 }
 
+//TODO Rename this and move to rock_motor.c:
 void qualify(void)
 {
   debugVar = locationWhereAmI();
   doUSB();
   m_wait(1);
   calculateAngleToGoal();
+  steeringAlgorithm();
 //TODO negativeError = max(255,min(0,k*(A1-A2+schmitt)));
 //TODO positiveError = max(255,min(0,k*(A2-A1+schmitt)));
 //TODO Modulo at the limits!
@@ -52,4 +71,32 @@ void qualify(void)
 // ERRORANGLE=-60  LEFT=0.00  RIGHT=0.50
   if (FALSE/*TODO*/) { /*MOTORLEFTPWM  = negativeError;*/ }
   if (FALSE/*TODO*/) { /*MOTORRIGHTPWM = positiveError;*/ }
+}
+
+void steeringAlgorithm(void)
+{
+  //TODO Use proper variable names!
+  degErrTurnCW  = robotAngle -  goalAngle - ANGLE_ERROR_TO_START_TURN;
+  degErrTurnCCW =  goalAngle - robotAngle - ANGLE_ERROR_TO_START_TURN;
+  degErrTurnCW  = max(0,min(MAX_ANGLE_ERROR,degreeErrorTurnCW ));
+  degErrTurnCCW = max(0,min(MAX_ANGLE_ERROR,degreeErrorTurnCCW));
+  //TODO Double check if this algorithm changes appropriate PWM:
+  //Calculate amount to SUBTRACT from each motor PWM:
+  motorR =     SLOW_WHEEL_SPEED_PER_DEG * degErrTurnCCW;
+  motorL =     SLOW_WHEEL_SPEED_PER_DEG * degErrTurnCW;
+  motorR = max(FAST_WHEEL_SPEED_PER_DEG * degErrTurnCW, motorR);
+  motorL = max(FAST_WHEEL_SPEED_PER_DEG * degErrTurnCCW,motorL);
+  motorR = max(0,FULL_SPEED - motorR);
+  motorL = max(0,FULL_SPEED - motorL);
+
+  //TODO Decrease motorR and motorL proportionately w/ goal distance!
+}
+
+void calculateAngleToGoal(void)
+{
+#define GOAL_A_X     911
+#define GOAL_A_Y     383
+#define GOAL_B_X     112
+#define GOAL_B_Y     383
+  
 }
--- rock_motor.h
+++ rock_motor.h
@@ -1,15 +1,20 @@
 #ifndef _rock_motor_h
 #define _rock_motor_h
 
-#define FORWARD       1
-#define REVERSE      -1
-#define LEFTMOTOR     1
-#define RIGHTMOTOR    2
+#define FORWARD                          1
+#define REVERSE                         -1
+#define LEFTMOTOR                        1
+#define RIGHTMOTOR                       2
 #ifndef OFF
-#define OFF           0
+#define OFF                              0
 #endif
-#define QTR_SPEED    63
-#define FULL_SPEED  255
+#define QTR_SPEED                       65
+#define HALF_SPEED                     130
+#define FULL_SPEED                     255
+#define SLOW_WHEEL_SPEED_PER_DEG         5
+#define FAST_WHEEL_SPEED_PER_DEG        10  
+#define ANGLE_ERROR_TO_START_TURN        2
+#define MAX_ANGLE_ERROR                 25
 
 void motor(unsigned char motor_id, char motor_dir, unsigned char motor_duty);
 void testMotors();
